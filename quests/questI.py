792cafb8355f34dc297a1970c3f5160398d6e89e
guillherme14 <guilherme_ok@hotmail.com>
2023-10-13 23:05:48 -0300

    final commit

---
+++ quests/questI.py
@@ -0,0 +1,8 @@
+# O método de busca em questão tem a função de retornar documentos baseando-se em um critério de busca específico:
+
+
+#O campo onde possui "cuisine" tem a função de retornar valores que não sejam igual a "American"
+
+#O campo "grades" consulta valores que sejam maior que 70
+
+#E o campo address.coord busca valores que estejam abaixo dos "65", provavelmente com o intuito de achar uma localização abaixo de determinada região no mapa
